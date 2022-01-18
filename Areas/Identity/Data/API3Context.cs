8282b15c6ccd4d897b05578ba241cbbafa0de2e9
AhmadAttalhaoui <61194391+AhmadAttalhaoui@users.noreply.github.com>
2022-01-18 17:29:36 +0100

    Add project files.

---
+++ Areas/Identity/Data/API3Context.cs
@@ -0,0 +1,25 @@
+﻿using API3.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using API3.Models;
+
+namespace API3.Data;
+
+public class API3Context : IdentityDbContext<API3User>
+{
+    public API3Context(DbContextOptions<API3Context> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+
+    public DbSet<API3.Models.User> User { get; set; }
+}
---
+++ Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API3.Models
+{
+    public class User
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Voornaam")]
+        public string Voornaam { get; set; }
+
+        [Required]
+        [Display(Name = "Achternaam")]
+        public string Achternaam { get; set; }
+
+        [Required]
+        [Display(Name = "Geboren op")]
+        [DataType(DataType.Date)]
+        public DateTime Geboortedatum { get; set; }
+    }
+
+    public class UserIndexViewModel
+    {
+        public int SelectedItem { get; set; }
+        public string SearchField { get; set; }
+        public List<User> Users { get; set; }
+
+    }
+}
